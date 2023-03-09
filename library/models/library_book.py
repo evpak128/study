1636b31843531566ee61383784b2883a8e226b86
evpak128 <evpak128@gmail.com>
2023-03-09 13:07:28 +0200

    [IMP]library: added demo data

---
+++ library/models/library_book.py
@@ -0,0 +1,33 @@
+from odoo import api, fields, models
+
+
+class LibraryBook(models.Model):
+    _name = 'library.book'
+    _description = 'Library Books'
+
+    name = fields.Char('Title')
+    reader_id = fields.Many2one(
+        comodel_name='res.partner',
+        string='Reader',
+    )
+    taken_date = fields.Date('In use from', readonly=True)
+    user_id = fields.Many2one(
+        comodel_name='res.users',
+        string='Responsible',
+    )
+    active = fields.Boolean(
+        string='Active',
+        default=True,
+    )
+    category_id = fields.Many2one(comodel_name='library.book.category')
+
+    @api.onchange('reader_id')
+    def _onchange_reader_id(self):
+        if self.reader_id and not self.taken_date:
+            self.taken_date = fields.Date.today()
+
+    def action_assign_default(self):
+        self.ensure_one()
+        self.reader_id = self.env.ref(
+            'library.res_partner_customer').id
+
